29b85a2eb98ccc95630ef78f74f027b0dfb3659f
PK <2023412heng@aupp.edu.kh>
2026-05-01 11:23:48 +0700

    feat: user management routes and views

--- server.js
+++ server.js
@@ -8,14 +8,20 @@ import apiRouter from './src/routes/api.routes.js'
 import clientRouter from './src/routes/client.routes.js'
 
 app.disable('x-powered-by')
+import usersRouter from './src/routes/users.js'
+import transactionsRouter from './src/routes/transactions.js'
 
 app.use(auditLogger)
 app.use(corsMiddleware)
 app.use(rateLimitMiddleware)
 app.use(express.json({ limit: '20mb' }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', apiRouter)
 app.use('/', clientRouter)
+app.use('/users', usersRouter)
+app.use('/transactions', transactionsRouter)
 
 const port = process.env.PORT
 
