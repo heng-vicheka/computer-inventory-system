cf235b974803cfe7df620ede2c55873cc790d9bc
PK <2023412heng@aupp.edu.kh>
2026-05-01 11:23:48 +0700

    feat: user management routes and views

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 import 'dotenv/config'
+import express from 'express'
 import app from './src/config/view.js'
 import corsMiddleware from './src/config/cors.js'
 import rateLimitMiddleware from './src/config/rateLimit.js'
 import auditLogger from './src/config/morgan.js'
 import apiRouter from './src/routes/api.js'
+import usersRouter from './src/routes/users.js'
+import transactionsRouter from './src/routes/transactions.js'
 
 app.use(auditLogger)
 app.use(corsMiddleware)
 app.use(rateLimitMiddleware)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', apiRouter)
+app.use('/users', usersRouter)
+app.use('/transactions', transactionsRouter)
 
 const port = process.env.PORT || 3000
 
