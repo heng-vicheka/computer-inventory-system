8ba8a2bf64735754820483aa6fbcab0b1bf62f22
PK <2023412heng@aupp.edu.kh>
2026-05-01 11:23:48 +0700

    feat: user management routes and views

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import express from 'express'
 import app from './src/config/view.js'
 import corsMiddleware from './src/config/cors.js'
 import rateLimitMiddleware from './src/config/rateLimit.js'
@@ -7,13 +8,19 @@ import apiRouter from './src/routes/api.routes.js'
 import clientRouter from './src/routes/client.routes.js'
 
 app.disable('x-powered-by')
+import usersRouter from './src/routes/users.js'
+import transactionsRouter from './src/routes/transactions.js'
 
 app.use(auditLogger)
 app.use(corsMiddleware)
 app.use(rateLimitMiddleware)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', apiRouter)
 app.use('/', clientRouter)
+app.use('/users', usersRouter)
+app.use('/transactions', transactionsRouter)
 
 const port = process.env.PORT || 3000
 
